bedb0fdfa5eed3b21acf3806b998ced5480ad724
Mark Grey <mark@mark-MS-7376.(none)>
2012-05-10 16:53:13 -0400

    added demonstration of mimetype

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ $f = new Forwardr('www.google.com');
 //to be returned back to this request
 $f->set_headers = True;
 
+//Used in conjuction with set_headers.  If you know the remote mimetype, set it here
+//to match in local response
+$f->mimetype = 'text/html';
+
 //Set to true to tell the internal curl not to fail on an HTTPError
 $f->debug = True;
 
